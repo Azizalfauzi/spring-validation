0960aece4ebfd97a8ce1fe38339ffb9bf12840a9
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-24 21:08:46 +0700

    *Spring validation - Configuration properties

--- src/main/java/zuhaprogrammer/spring/validation/SpringValidationApplication.java
+++ src/main/java/zuhaprogrammer/spring/validation/SpringValidationApplication.java
@@ -2,12 +2,15 @@ package zuhaprogrammer.spring.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import zuhaprogrammer.spring.validation.properties.DatabaseProperties;
 
+@EnableConfigurationProperties({DatabaseProperties.class})
 @SpringBootApplication
 public class SpringValidationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringValidationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationApplication.class, args);
+    }
 
 }
