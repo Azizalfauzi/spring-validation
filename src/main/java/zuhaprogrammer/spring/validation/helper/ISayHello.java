6e0cd14ae3a0bad4c431593b67e3df1bf8e623ca
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-24 21:02:31 +0700

    *Spring validation - Method overriding

---
+++ src/main/java/zuhaprogrammer/spring/validation/helper/ISayHello.java
@@ -0,0 +1,10 @@
+package zuhaprogrammer.spring.validation.helper;
+
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public interface ISayHello {
+    String sayHello(@NotBlank String name);
+}
